f8775ad6d1c66edefd9be27d717d5b8ee8144202
greg <greg@example.com>
2020-03-30 20:04:36 +0200

    init commit

---
+++ src/main/java/pl/filewicz/actorapi/ActorApiApplication.java
@@ -0,0 +1,13 @@
+package pl.filewicz.actorapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActorApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActorApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/filewicz/actorapi/ActorApiApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.filewicz.actorapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActorApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
